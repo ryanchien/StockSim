f32e1b817f01264b07606ac457d7bcfb63a995ce
Ryan Chien <ryanchien@wirelessprv-10-194-91-141.near.illinois.edu>
2019-10-28 20:28:14 -0500

    using forms

--- webcode/source/main/views.py
+++ webcode/source/main/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
+import accounts.forms
+import requests
 
 class IndexPageView(TemplateView):
     template_name = 'main/index.html'
 
+    def stocks(request):
+    	stocks_result = form.get_stocks()
+    	return render(request, 'main/index.html', {'form': form, 'stocks_result': stocks_result})
 
 class ChangeLanguageView(TemplateView):
     template_name = 'main/change_language.html'
