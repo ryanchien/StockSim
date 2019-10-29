f4fc36fa962c09466d353066df005e3e1593a462
Ryan Chien <ryanchien@wirelessprv-10-194-91-141.near.illinois.edu>
2019-10-28 19:47:38 -0500

    Revert "grabbing data from worldtradingdata"
    This reverts commit 17154159930d8ebb0b755cbf0088e18b074ae715.

--- webcode/source/main/views.py
+++ webcode/source/main/views.py
@@ -1,18 +1,9 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
-import requests
+
 
 class IndexPageView(TemplateView):
-	template_name = 'main/index.html'
-
-	def home(request):
-		par = {'symbol':'SNAP', 'api_token':'svaMvA7fFajd6B3EBsfrLZL6lCfmqLl6vJgCbGPBisqN74QPzH3kF49JDqR4'}
-		response = requests.get(url = 'https://api.worldtradingdata.com/api/v1/stock', params = par)
-		stock = response.json() if response and response.status_code == 200 else None
-		if stock and 'symbol' in stock:
-			dic = json.loads(stock)
-			return render(request, 'main/home.html', dic['data'][0])
+    template_name = 'main/index.html'
 
 
 class ChangeLanguageView(TemplateView):
-	template_name = 'main/change_language.html'
+    template_name = 'main/change_language.html'
