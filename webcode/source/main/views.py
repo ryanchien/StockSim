f3e856d95079dbc56c65d851a6593cc498143925
derek <derekniess96@gmail.com>
2019-10-28 21:29:50 -0500

    Pass in variables to html templates works

--- webcode/source/main/views.py
+++ webcode/source/main/views.py
@@ -5,14 +5,11 @@ import requests
 class IndexPageView(TemplateView):
 	template_name = 'main/index.html'
 
-	def home(request):
-		par = {'symbol':'SNAP', 'api_token':'svaMvA7fFajd6B3EBsfrLZL6lCfmqLl6vJgCbGPBisqN74QPzH3kF49JDqR4'}
-		response = requests.get(url = 'https://api.worldtradingdata.com/api/v1/stock', params = par)
-		stock = response.json() if response and response.status_code == 200 else None
-		if stock and 'symbol' in stock:
-			dic = json.loads(stock)
-			return render(request, 'main/home.html', dic['data'][0])
-
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['symbol'] = 'APPL'
+		return context
+	
 
 class ChangeLanguageView(TemplateView):
 	template_name = 'main/change_language.html'
