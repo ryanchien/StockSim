7c399acd8b77b9c0fa26e1f3d40c164e6cb7c263
Ryan Chien <ryanchien@wirelessprv-10-194-91-141.near.illinois.edu>
2019-10-29 22:28:01 -0500

    form working

--- webcode/source/main/views.py
+++ webcode/source/main/views.py
@@ -1,10 +1,25 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-import accounts.forms
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.crypto import get_random_string
+from django.utils.decorators import method_decorator
+from django.utils.http import is_safe_url
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import View, FormView
+from django.conf import settings
 import requests
-import common.db_helper
+from django.urls import reverse
 
-class IndexPageView(TemplateView):
+from django.views.generic import View, FormView
+from .forms import StocksForm
+from django.shortcuts import get_object_or_404, redirect
+
+class IndexPageView(TemplateView, FormView):
 	template_name = 'main/index.html'
 
 	@staticmethod
