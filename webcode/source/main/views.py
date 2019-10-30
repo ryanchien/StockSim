9e8ff6998bc0dbd051c1e5fd232169eb20c6dcd2
Pierre Motard <p.motard@outlook.com>
2019-10-29 23:26:07 -0500

    Revert "Merge branch 'master' of https://github.com/peterz3/StockSim"
    This reverts commit a4d61b61c840311a4f14e7c9936e787987a38cd5, reversing
    changes made to 9b4321aaa3bbd3fdfff8c0ab284109bcc60b442b.

--- webcode/source/main/views.py
+++ webcode/source/main/views.py
@@ -1,46 +1,12 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404, redirect
-from django.utils.crypto import get_random_string
-from django.utils.decorators import method_decorator
-from django.utils.http import is_safe_url
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.utils.translation import gettext_lazy as _
-from django.views.decorators.cache import never_cache
-from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.debug import sensitive_post_parameters
-from django.views.generic import View, FormView
-from django.conf import settings
+import accounts.forms
 import requests
-from django.urls import reverse
+import common.db_helper
 
-from django.views.generic import View, FormView
-from .forms import StocksForm
-from django.shortcuts import get_object_or_404, redirect
-
-class IndexPageView(TemplateView, FormView):
+class IndexPageView(TemplateView):
 	template_name = 'main/index.html'
 
-	@staticmethod
-	def get_form_class(**kwargs):
-		return StocksForm
-
-	def form_valid(self, form):
-		request = self.request
-		return redirect('index', symbol=form.cleaned_data['stockdata'])
-
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-
-		if self.request.get_full_path() == '/':
-			context['symbol'] = ''
-		else:
-			url = self.request.get_full_path()
-			context['symbol'] = url.split('?stockdata=')[1]
-		return context
-
-'''
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		
@@ -55,9 +21,6 @@ class IndexPageView(TemplateView, FormView):
 			context['symbol'] = 'Does not exist'
 
 		return context
-'''
-
-
 
 
 class ChangeLanguageView(TemplateView):
