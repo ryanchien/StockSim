a710b3a7d125bb72e2f2756a0e5c13ea3120fef9
derek <derekniess96@gmail.com>
2019-12-04 10:26:45 -0600

    Finished Buy limit order setup and TODO

--- webcode/source/limit-order-trigger.sql
+++ webcode/source/limit-order-trigger.sql
@@ -1,3 +1,26 @@
+-- TODO: 
+    -- 1) in the front-end, need a button to insert an open limit buy order to TradingHistory.
+    -- This will involve: 
+        -- checking if the available USD quantity the user has is sufficient for order quantity
+        -- updating Portfolios with subtracted USD of buy order (could be turned into a trigger)
+        -- finally, inserting open buy order to TradingHistory
+    
+        -- for open sell orders, similar process:
+            -- checking if the available Stock quantity user has is sufficient for order quantity
+            -- updating Portfolios with subtracted Stock of sell order (could be turned into a trigger)
+            -- finally, inserting open sell order to TradingHistory
+    
+    -- 2) in front-end, also need a button to delete an open limit buy order from TradingHistory.
+        -- This will involve:
+            -- checking if the record exists first. HOW WILL WE QUERY FOR THIS RECORD? Can we get the record ID
+                -- from the front-end? (User, Symbol) wouldn't work as a key because user can have multiple orders on
+                -- same symbol
+            -- proceeding with delete statement on this record if so
+
+        -- for open sell orders, exact same process.
+
+    -- 3) Could add condition where old.OpenOrder = new.OpenOrder to avoid duplicating updates if this becomes a problem
+
 -- Closes newly filled orders to activate other triggers that update db with this new info
 CREATE TRIGGER CloseLimitOrder
     AFTER UPDATE OF Value ON Stocks
@@ -11,8 +34,9 @@ CREATE TRIGGER CloseLimitOrder
               new.Value <= AskingPrice;
     END;
 
--- Cascaded from CloseLimitOrder trigger
-CREATE TRIGGER ApplyClosedBuyOrdersToNew
+-- Cascaded from CloseLimitOrder trigger, apply filled buy order to portfolio 
+--      quantity previously 0, so must create new record
+CREATE TRIGGER ApplyClosedBuyOrdersToNewRow
     AFTER UPDATE OF OpenOrder ON TradingHistory
     WHEN (old.BuySell = 'B' AND NOT EXISTS (SELECT * FROM Portfolios WHERE Symbol = old.Symbol AND Username = old.User))
     BEGIN
@@ -20,8 +44,9 @@ CREATE TRIGGER ApplyClosedBuyOrdersToNew
             VALUES (old.User, old.Symbol, old.Quantity);
     END;
 
--- Cascaded from CloseLimitOrder trigger        
-CREATE TRIGGER ApplyClosedBuyOrdersToExisting
+-- Cascaded from CloseLimitOrder trigger, apply filled buy order to portfolio
+--      record exists, so add order quantity to current quantity
+CREATE TRIGGER ApplyClosedBuyOrdersToExistingRow
     AFTER UPDATE OF OpenOrder ON TradingHistory
     FOR EACH ROW
     WHEN (old.BuySell = 'B' AND EXISTS (SELECT * FROM Portfolios WHERE Symbol = old.Symbol AND Username = old.User))
@@ -31,8 +56,57 @@ CREATE TRIGGER ApplyClosedBuyOrdersToExisting
             WHERE old.Symbol = Symbol AND
                   old.User = Username;
     END;
-        
+
+-- When limit buy order is cancelled, want to reimburse escrow USD of user
+CREATE TRIGGER ApplyCancelledBuyOrdersToExistingRow
+    AFTER DELETE ON TradingHistory
+    FOR EACH ROW
+    WHEN (old.BuySell = 'B' AND EXISTS (SELECT * FROM Portfolios WHERE Symbol = 'USD' AND Username = old.User))
+    BEGIN
+        UPDATE Portfolios
+            SET Quantity = Quantity + (old.Quantity * old.AskingPrice)
+            WHERE Username = old.User AND
+                  Symbol = 'USD';
+    END;
+
+-- When limit buy order is cancelled, want to reimburse escrow USD of user
+CREATE TRIGGER ApplyCancelledBuyOrdersToNewRow
+    AFTER DELETE ON TradingHistory
+    WHEN (old.BuySell = 'B' AND NOT EXISTS (SELECT * FROM Portfolios WHERE Symbol = 'USD' AND Username = old.User))
+    BEGIN
+        INSERT INTO Portfolios
+            VALUES (old.User, 'USD', (old.Quantity * old.AskingPrice));
+    END;
+
+-- Would we need to have a separate trigger for when we make an order resulting in 0 USD in portfolio?
+-- Since the logically invalid buy order (order where user has insufficient funds) will be handled in backend, 
+    -- only need to consider equal to as an extra case
+CREATE TRIGGER SubtractBuyOrderCostToEmpty
+    BEFORE INSERT ON TradingHistory
+    WHEN new.BuySell = 'B' AND 
+         new.OpenOrder = 1 AND 
+         (new.AskingPrice * new.Quantity) = (SELECT Quantity FROM Portfolios WHERE Username = new.User AND Symbol = new.Symbol)
+    BEGIN
+        DELETE FROM Portfolios
+            WHERE Username = new.User AND Symbol = 'USD';
+    END;
+
+CREATE TRIGGER SubtractBuyOrderCost
+    BEFORE INSERT ON TradingHistory
+    WHEN new.BuySell = 'B' AND 
+         new.OpenOrder = 1 AND 
+         (new.AskingPrice * new.Quantity) <> (SELECT Quantity FROM Portfolios WHERE Username = new.User AND Symbol = new.Symbol)
+    BEGIN
+        UPDATE Portfolios
+            SET Quantity = Quantity - (new.AskingPrice * new.Quantity)
+            WHERE Username = new.User AND 
+                  Symbol = 'USD';
+    END;
+
+
+
 CREATE TABLE TradingHistory (
+TimePurchased DATE,
 User CHAR(256),
 Symbol Char(256),
 AskingPrice FLOAT,
